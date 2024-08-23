122b4efcb1ae3f5662a046639543017e774e2953
Rakshit879 <grakshit430@gmail.com>
2024-08-23 15:49:06 +0530

    function based but not news component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import './App.css';
-import React,{Component} from 'react';
+import React, { useState } from 'react';
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component{
-  PageSize=15;
-  apiKey= process.env.REACT_APP_NEWS_API
-  state={
-    progress:0
+export default function App() {
+  let PageSize = 15;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const [progresss, setProgresss] = useState(0);
+  // state={
+  //   progress:0
+  // }
+  const setProgress = (progresss) => {
+    // this.setState({progress:progress})
+    setProgresss({ progresss: progresss });
   }
-  setProgress=(progress)=>{
-    this.setState({progress:progress})
-  }
-  render(){
-    return(
-      <div>
-        <BrowserRouter>
-        <Navbar/>
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
         <LoadingBar
-        color='#f11946'
-        height={3}
-        progress={this.state.progress}
-      />
+          color='#f11946'
+          height={3}
+          progress={progresss}
+        />
         <Routes>
-          <Route exact path='/'element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="apple" PageSize={this.PageSize} search="apple"/>}></Route>
-          <Route exact path='/business' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" PageSize={this.PageSize} search="business"/>}></Route>
-          <Route exact path='/entertainment' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" PageSize={this.PageSize} search="entertainment"/>}></Route>
-          <Route exact path='/general' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" PageSize={this.PageSize} search="general"/>}></Route>
-          <Route exact path='/health' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" PageSize={this.PageSize} search="health"/>}></Route>
-          <Route exact path='/science' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" PageSize={this.PageSize} search="science"/>}></Route>
-          <Route exact path='/sports' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" PageSize={this.PageSize} search="sports"/>}></Route>
-          <Route exact path='/technology' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" PageSize={this.PageSize} search="technology"/>}></Route>
+          <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey} key="apple" PageSize={PageSize} search="apple" />}></Route>
+          <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" PageSize={PageSize} search="business" />}></Route>
+          <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" PageSize={PageSize} search="entertainment" />}></Route>
+          <Route exact path='/general' element={<News setProgress={setProgress} apiKey={apiKey} key="general" PageSize={PageSize} search="general" />}></Route>
+          <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" PageSize={PageSize} search="health" />}></Route>
+          <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" PageSize={PageSize} search="science" />}></Route>
+          <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" PageSize={PageSize} search="sports" />}></Route>
+          <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" PageSize={PageSize} search="technology" />}></Route>
         </Routes>
-        </BrowserRouter>
-      </div>
-    )
-  }
+      </BrowserRouter>
+    </div>
+  )
+
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class NewsItem extends Component {
-    constructor() {
-        super();
-    }
-    render() {
-        let { title, description, newsURL, author, date, source } = this.props;
-        return (
-            <div className='my-3'>
-                <div className="card" >
-                    <div style={{display:"flex",justifyContent:"flex-end",position:"absolute",right:"0"}}>
-                    <span className="badge rounded-pill bg-danger">{source}</span>
-                    </div>
-                    <img src={this.props.imageURL} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title ? (title.length > 20 ? title.slice(0, 45) + "..." : title) : "Default Title"}</h5>
-                        <p className="card-text">{description ? (description.length > 30 ? description.slice(0, 88) + "..." : description) : "Default Description"}</p>
-                        <p className="card-text"><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toUTCString()}</small></p>
-                        <a href={newsURL} target='_blank' rel="noreferrer"className="btn btn-sm btn-dark">Read More</a>
-                    </div>
+export default function NewsItem(props) {
+    return (
+        <div className='my-3'>
+            <div className="card" >
+                <div style={{ display: "flex", justifyContent: "flex-end", position: "absolute", right: "0" }}>
+                    <span className="badge rounded-pill bg-danger">{props.source}</span>
+                </div>
+                <img src={props.imageURL} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{props.title ? (props.title.length > 20 ? props.title.slice(0, 45) + "..." : props.title) : "Default Title"}</h5>
+                    <p className="card-text">{props.description ? (props.description.length > 30 ? props.description.slice(0, 88) + "..." : props.description) : "Default Description"}</p>
+                    <p className="card-text"><small className='text-muted'>By {!props.author ? "Unknown" : props.author} on {new Date(props.date).toUTCString()}</small></p>
+                    <a href={props.newsURL} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
